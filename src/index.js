7567d7f97dac3494ffbab0b585183811060ad2be
marcuscaum <marcuslucas.jb@gmail.com>
2019-05-31 19:36:01 -0300

    Add Apollo and Material UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
 import './index.css';
 import App from './pages/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'https://api.thegraph.com/subgraphs/name/graphprotocol/uniswap',
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
